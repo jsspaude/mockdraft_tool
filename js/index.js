799a4a3d78bb0911b1671f65ddb909c189b67680
Jess Spaude <jsspaude@gmail.com>
2020-03-16 10:21:36 -0700

    two dbs

--- js/index.js
+++ js/index.js
@@ -1,20 +1,19 @@
 // Create needed constants
-const   table           = document.querySelector('[data-js="playerTable"]'),
-        settings        = document.querySelector('[data-js="settings"]'),
-        managers        = document.querySelector('[data-js="managerContainer"]'),
-        namesButton     = settings.querySelector('button'),
-        formStart       = document.querySelector('[data-js="startDraft"]'),
-        formSetup       = document.querySelector('[data-js="setupDraft"]'),
-        managerInput    = document.querySelectorAll('[data-js="managerName"]');
-var     teamCount       = 0;
-var     button          = document.querySelectorAll('[data-js="teamCount"');
-let     db;
-let     db2;
-let     players;
-let     requestURL      = 'js/draft_data.json';
-let     request         = new XMLHttpRequest();
-
-console.log(namesButton);
+const   playerTable         = document.querySelector('[data-js="playerTable"]'),
+        managerContainer    = document.querySelector('[data-js="managerContainer"]'),
+        settings            = document.querySelector('[data-js="settings"]'),
+        rounds              = settings.querySelector('[data-js="numOfRounds"]'),
+        namesButton         = settings.querySelector('button'),
+        formStart           = document.querySelector('[data-js="startDraft"]'),
+        formSetup           = document.querySelector('[data-js="setupDraft"]');
+var     teamCount           = 0,
+        button              = document.querySelectorAll('[data-js="teamCount"');
+let     db,
+        db2,
+        managerInput,
+        players;
+        requestURL          = 'js/draft_data.json';
+        request             = new XMLHttpRequest();
 
 request.open('GET', requestURL);
 request.responseType = 'json';
@@ -42,19 +41,13 @@ window.onload = function() {
     // onsuccess handler signifies that the database opened successfully
     request.onsuccess = function() {
         console.log('Draft data inputted');
-    
         // Store the opened database object in the db variable. This is used a lot below
         db = request.result;
-        // Run the displayData() function to display the notes already in the IDB
-    //     displayData();
     };
     request2.onsuccess = function() {
-        console.log('Draft data inputted');
-    
+        console.log('Manager data inputted');
         // Store the opened database object in the db variable. This is used a lot below
         db2 = request2.result;
-        // Run the displayData() function to display the notes already in the IDB
-    //     displayData();
     };
     // Setup the database tables if this has not already been done
     request.onupgradeneeded = function(e) {
@@ -67,6 +60,7 @@ window.onload = function() {
     
         console.log('Database setup complete');
     };
+
     request2.onupgradeneeded = function(e) {
         // Grab a reference to the opened database
         let db2 = e.target.result;
@@ -105,8 +99,8 @@ window.onload = function() {
         transaction.oncomplete = function() {
             console.log('Transaction completed: database modification finished.');
     
-        // update the display of data to show the newly added item, by running displayData() again.
-            displayData();
+        // update the display of data to show the newly added item, by running displayPlayers().
+            displayPlayers();
         };
     
         transaction.onerror = function() {
@@ -118,35 +112,39 @@ window.onload = function() {
         e.preventDefault();
 
         // grab the values entered into the form fields and store them in an object ready for being inserted into the DB
-        let newItem = { teamName: teamNameInput.value };
-    
-        // open a read/write db transaction, ready for adding the data
-        let transaction2 = db2.transaction(['managerStore'], 'readwrite');
-    
-        // call an object store that's already been added to the database
-        let managerStore = transaction2.objectStore('managerStore');
-    
-        // Make a request to add our newItem object to the object store
-        let request = objectStore.add(newItem);
-        request.onsuccess = function() {
-        // Clear the form, ready for adding the next entry
-        teamNameInput.value = '';
-        };
-    
-        // Report on the success of the transaction completing, when everything is done
-        transaction2.oncomplete = function() {
-            console.log('Transaction completed: database modification finished.');
-    
-        // update the display of data to show the newly added item, by running displayData() again.
-            // displayData();
+
+        managerInput.forEach((manager) => {
+
+            let newItem = { managerValue: manager.value };
+        
+            // open a read/write db transaction, ready for adding the data
+            let transaction2 = db2.transaction(['managerStore'], 'readwrite');
+        
+            // call an object store that's already been added to the database
+            let managerStore = transaction2.objectStore('managerStore');
+        
+            // Make a request to add our newItem object to the object store
+            let request = managerStore.add(newItem);
+            request.onsuccess = function() {
+        
+            };
+
+            // Report on the success of the transaction completing, when everything is done
+            transaction2.oncomplete = function() {
+                console.log('Transaction completed: database modification finished.');
+        
+            // update the display of data to show the newly added item, by running displayManagers().
+                displayManagers();
         };
     
         transaction2.onerror = function() {
         console.log('Transaction not opened due to error');
         };
+
+        });
     }
-    // Define the displayData() function
-    function displayData() {
+
+    function displayPlayers() {
         // Open our object store and then get a cursor - which iterates through all the
         // different data items in the store
 
@@ -169,11 +167,67 @@ window.onload = function() {
             tableRow.appendChild(adpData);
             tableRow.appendChild(nameData);
             tableRow.appendChild(teamData);
-            table.appendChild(tableRow);
+            playerTable.appendChild(tableRow);
     
             // Put the data from the cursor inside the h3 and para
             
             adpData.textContent     = cursor.value.adp;
+            nameData.textContent    = cursor.value.name;
+            teamData.textContent    = cursor.value.team;
+
+            // Store the ID of the data item inside an attribute on the tableRow, so we know
+            // which item it corresponds to. This will be useful later when we want to delete items
+            tableRow.setAttribute('data-playerKey', cursor.value.id);
+            tableRow.setAttribute('data-manager', 0);
+    
+            // Create a button and place it inside each tableRow
+            const draftBtn = document.createElement('button');
+            tableRow.appendChild(draftBtn);
+            draftBtn.textContent = 'DRAFT';
+    
+            draftBtn.onclick = draftPlayer;
+    
+            // Iterate to the next item in the cursor
+            cursor.continue();
+        } 
+        else {
+            // if there are no more cursor items to iterate through, say so
+            console.log('Notes all displayed');
+        }
+        };
+    }
+
+    function displayManagers() {
+        // Open our object store and then get a cursor - which iterates through all the
+        // different data items in the store
+
+        let managerStore = db.transaction('managerStore').objectStore('managerStore');
+        managerStore.openCursor().onsuccess = function(e) {
+
+        // Get a reference to the cursor
+        let cursor = e.target.result;
+
+        // If there is still another data item to iterate through, keep running this code
+        if(cursor) {
+
+            // Create a list item, h3, and p to put each data item inside when displaying it
+            // structure the HTML fragment, and append it inside the list
+            const article       = document.createElement('article');
+            const table         = document.createElement('table');
+            const managerData   = document.createElement('th');
+            const draftRow      = document.createElement('tr');
+            const draftData     = document.createElement('td');
+
+    
+            article.appendChild(table);
+            table.appendChild(managerData);
+            table.appendChild(draftRow);
+            draftRow.appendChild(draftData);
+            managerContainer.appendChild(article);
+    
+            // Put the data from the cursor inside the h3 and para
+            
+            managerData.textContent     = cursor.value.adp;
             nameData.textContent    = cursor.value.name;
             teamData.textContent    = cursor.value.team;
 
@@ -208,12 +262,15 @@ window.onload = function() {
         const   teamCount = settings.querySelector('select').value;
 
         for(let i=0; i < teamCount; i++) {
-            var managerInput = document.createElement('input');
-            formSetup.appendChild(managerInput);
-            managerInput.setAttribute('placeholder', `Manager ${i}`);
-            managerInput.setAttribute('data-manager', i);
-            managerInput.setAttribute('data-js', 'managerName');
+            var managerName = document.createElement('input');
+            formSetup.appendChild(managerName);
+            managerName.setAttribute('placeholder', `Manager ${i}`);
+            managerName.setAttribute('data-manager', i);
+            managerName.setAttribute('data-js', 'managerName');
             console.log('test');
         }
+
+        managerInput = document.querySelectorAll('[data-js="managerName"]');
+        console.log(managerInput);
     }
 };

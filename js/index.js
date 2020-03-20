823fe4ea828aacfec8634153a756e30f9f678c65
Jess Spaude <jsspaude@gmail.com>
2020-03-19 22:46:25 -0700

    manager display/info display fixed

--- js/index.js
+++ js/index.js
@@ -4,15 +4,16 @@ const   playerTable             = document.querySelector('[data-js="playerTable"
         settings                = document.querySelector('[data-js="settings"]'),
         namesButton             = settings.querySelector('[data-js="manNameBtn"]'),
         resetButton             = settings.querySelector('[data-js="resetBtn"]'),
-        positions               =['QB','RB','RB','WR','WR','TE','FLEX','DST','K'],
+        positions               =['qb','rb','rb','wr','wr','te','flex','dst','k'],
         roundTracker            = document.querySelector('[data-js="roundTracker"]'),
         managerTracker          = document.querySelector('[data-js="managerTracker"]'), 
         numRounds               = settings.querySelector('[data-js="numRounds"]'),
         formStart               = document.querySelector('[data-js="startDraft"]');
 var     managerCount,
         managerInput,
-        managerDataArray        = [],
-        button                  = document.querySelectorAll('[data-js="teamCount"');
+        currManager,
+        currRound,
+        currManagerName;
 let     db,
         db2,
         players;
@@ -29,7 +30,6 @@ request.onload = function() {
 
 window.onload = function() {
     let request = window.indexedDB.open('mockDraft', 1);
-
     request.onerror = function() {
         console.log('MockDraft Failed to Open');
     };
@@ -37,24 +37,34 @@ window.onload = function() {
         console.log('MockDraft Data Inputted');
         db = request.result;
 
+        displayInfo();
         displayManagers();
         displayPlayers();
-        displayInfo();
     };
     request.onupgradeneeded = function(e) {
         let db              = e.target.result,
             playerStore     = db.createObjectStore('playerStore', { keyPath: 'id', autoIncrement:true }),
             managerStore    = db.createObjectStore('managerStore', { keyPath: 'id', autoIncrement:true }),
             settingsStore   = db.createObjectStore('settingsStore', { keyPath: 'id', autoIncrement:true });
+            playerStore.createIndex('name', 'name', { unique: false });
+            playerStore.createIndex('drafted', 'drafted', { unique: false });
+            playerStore.createIndex('roundDrafted', 'roundDrafted', { unique: false });
+            playerStore.createIndex('manager', 'manager', { unique: false });
+            settingsStore.createIndex('numRounds', 'numRounds', { unique: false });
+            settingsStore.createIndex('currRound', 'currRound', { unique: false });
+            settingsStore.createIndex('currManager', 'currManager', { unique: false });
+            settingsStore.createIndex('numManagers', 'numManagers', { unique: false });
+            settingsStore.createIndex('tracker', 'tracker', { unique: false });
+            managerStore.createIndex('managerNum', 'managerNum', {unique: true});
         console.log('MockDraft Stores Added');
     };
 
     namesButton.addEventListener("click", initSettings);
     resetButton.addEventListener("click", () => {
         clearData();
+        displayInfo();
         displayManagers();
         displayPlayers();
-        displayInfo();
     });
     formStart.onsubmit = addData;
 
@@ -80,7 +90,7 @@ window.onload = function() {
             playerStore     = transaction.objectStore('playerStore'),
             managerStore    = transaction2.objectStore('managerStore'),
             settingsStore   = transaction3.objectStore('settingsStore'),
-            newItem = {numRounds: numRounds.value, currRound: 1, currManager: 0, numManagers: managerCount};
+            newItem = {numRounds: numRounds.value, currRound: 1, currManager: 0, numManagers: managerCount, tracker: 0};
             request = settingsStore.add(newItem);
 
         players.forEach(function(player){
@@ -95,9 +105,21 @@ window.onload = function() {
         };
         transaction.oncomplete = function() {
             console.log('Data Stores Updated');
-            displayPlayers();
-            displayManagers();
-            displayInfo();
+
+            const promiseA = new Promise( (resolve, reject) => {
+                resolve(displayInfo());
+            });
+
+            promiseA
+            .then(() => {
+                displayPlayers();
+            }) 
+            .then(() => {
+                displayManagers();
+            }) 
+            .catch(() => {
+                console.log('promiseA error')
+            }) 
         };
         transaction.onerror = function() {
         console.log('Data Store Error');
@@ -114,6 +136,7 @@ window.onload = function() {
                 const   tableRow                    = document.createElement('tr'),
                         adpData                     = document.createElement('td'),
                         nameData                    = document.createElement('td'),
+                        posData                    = document.createElement('td'),
                         teamData                    = document.createElement('td'),
                         updatePlayer                = cursor.value,
                         draftBtn                    = document.createElement('button');
@@ -128,10 +151,12 @@ window.onload = function() {
                 }
                 tableRow.appendChild(adpData);
                 tableRow.appendChild(nameData);
+                tableRow.appendChild(posData);
                 tableRow.appendChild(teamData);
                 playerTable.appendChild(tableRow);
                 adpData.textContent     = cursor.value.adp;
                 nameData.textContent    = cursor.value.name;
+                posData.textContent     = cursor.value.pos;
                 teamData.textContent    = cursor.value.team;
                 tableRow.setAttribute('data-playerKey', cursor.value.id);
                 tableRow.setAttribute('data-manager', 99);
@@ -144,21 +169,11 @@ window.onload = function() {
                 console.log('Player Data Displayed');
             }
         };
-
-        console.log(managerDataArray);
     }
 
     function displayManagers() {
         let managerStore    = db.transaction('managerStore').objectStore('managerStore'),
-            settingsStore   = db.transaction('settingsStore').objectStore('settingsStore'),
-            rounds          = "";
-        
-        settingsStore.openCursor().onsuccess = function(e) {
-            let cursor = e.target.result;
-            if(cursor) {
-                rounds = cursor.value.numRounds;
-            }
-        };
+            settingsStore   = db.transaction('settingsStore').objectStore('settingsStore');
 
         managerStore.openCursor().onsuccess = function(e) {
             let cursor = e.target.result;
@@ -166,25 +181,28 @@ window.onload = function() {
                 const   article     = document.createElement('article'),
                         table       = document.createElement('table'),
                         managerData = document.createElement('th');
+                var     draftData;
 
                 article.appendChild(table);
                 table.appendChild(managerData);
 
-                for(let i=0; i < rounds; i++) {
+                for(let i= 0; i < rounds; i++) {
                     const draftRow      = document.createElement('tr');
                     table.appendChild(draftRow);
 
-                    for(let i=0; i < 2; i++) {
-                        const draftData = document.createElement('td');
+                    for(let i= 0; i < 2; i++) {
+                        draftData = document.createElement('td');
                         draftRow.appendChild(draftData);
                         draftData.setAttribute('data-td', i);
                     }
 
                     if(i>8) {
                         draftRow.firstChild.innerText = 'BENCH';
+                        draftData.parentElement.children[1].setAttribute('data-pos', 'bench');
                     }
                     else{
-                        draftRow.firstChild.innerText = positions[i];
+                        draftRow.firstElementChild.innerText = positions[i].toUpperCase();
+                        draftData.parentElement.children[1].setAttribute('data-pos', positions[i]);
                     }
                 }
 
@@ -197,7 +215,6 @@ window.onload = function() {
                     managerData.innerText = cursor.value.managerName;
                 }
                 table.setAttribute('data-manager', cursor.value.managerNum);
-                managerDataArray.push(cursor.value.managerName)
                 cursor.continue();
             } 
             else {
@@ -207,24 +224,38 @@ window.onload = function() {
     }
 
     function displayInfo() {
-        let settingsStore   = db.transaction('settingsStore').objectStore('settingsStore');
+        let settingsTransaction     = db.transaction(["settingsStore"], "readwrite"),
+            settingsStore           = settingsTransaction.objectStore("settingsStore"),
+            settingsTrackerIndex    = settingsStore.index('tracker'),
+            settingsRequest         = settingsTrackerIndex.get(0);
+
+        settingsRequest.onsuccess = function(event) {
+            var data                    = event.target.result;
+                currRound               = data.currRound;
+                currManager             = data.currManager;
+                roundTracker.innerText  = currRound;
+
+            let managerTransaction      = db.transaction(["managerStore"], "readwrite"),
+                managerStore            = managerTransaction.objectStore("managerStore"),
+                managerNumIndex         = managerStore.index('managerNum'),
+                managerRequest          = managerNumIndex.get(currManager),
+                currName;
+
+            rounds = parseInt(data.numRounds);
+            managerRequest.onerror = function(event) {
+                console.log('Manager Tracker Update Error');
+            };
     
-        settingsStore.openCursor().onsuccess = function(e) {
-            let cursor = e.target.result;
-            
-            if(cursor) {
-                currRound   = cursor.value.currRound;
-                currManager = cursor.value.currManager;
-                if(managerDataArray[currManager] === "") {
+            managerRequest.onsuccess = function(event) {
+                var data = event.target.result;
+                currName = data.managerName;
+                if(currName === "") {
                     managerTracker.innerText = `Manager ${currManager}`;
                 }
                 else{
-                    managerTracker.innerText = managerDataArray[currManager];
+                    managerTracker.innerText = currName;
                 }
-                roundTracker.innerText = currRound;
-                roundTracker.setAttribute('data-round', currRound);
-                managerTracker.setAttribute('data-manager', currManager);
-            }
+            };  
         };
     }
 
@@ -245,30 +276,67 @@ window.onload = function() {
     };
 
     function draftPlayer(draftBtn) {
-        var playerStore     = db.transaction(["playerStore"], "readwrite").objectStore("playerStore");
-        var settingsStore   = db.transaction(["settingsStore"], "readwrite").objectStore("settingsStore");
-        var playerKey       = parseInt(draftBtn.target.parentElement.getAttribute('data-playerkey'));
-        var currManager     = document.querySelector('[data-manager]');
-        var currRound       = document.querySelector('[data-round]');
-        var request1        = playerStore.get(playerKey);
-        var playerTracker   = document.querySelector('[data-js="playerTracker"]');
+        let playerTransaction       = db.transaction(["playerStore"], "readwrite"),
+            playerStore             = playerTransaction.objectStore("playerStore"),
+            playerKey               = parseInt(draftBtn.target.parentElement.getAttribute('data-playerkey')),
+            playerRequest           = playerStore.get(playerKey),
+            playerTracker           = document.querySelector('[data-js="playerTracker"]'),
+            settingsTransaction     = db.transaction(["settingsStore"], "readwrite"),
+            settingsStore           = settingsTransaction.objectStore("settingsStore"),
+            settingsTrackerIndex    = settingsStore.index('tracker'),
+            settingsRequest         = settingsTrackerIndex.get(0);
 
-        request1.onerror = function(event) {
+        playerRequest.onerror = function(event) {
             console.log('Draft Button Error')
         };
-        request1.onsuccess = function(event) {
+        playerRequest.onsuccess = function(event) {
             var data = event.target.result;
-            data.manager = parseInt(currManager.getAttribute('data-manager'));
+            data.manager = currManager;
             data.drafted = 1;
-            data.roundDrafted = parseInt(currRound.getAttribute('data-round'));
+            data.roundDrafted = currRound;
 
-            var request1Update = playerStore.put(data);
-            request1Update.onerror = function(event) {
+            var requestUpdate = playerStore.put(data);
+            requestUpdate.onerror = function(event) {
                 console.log('Manager Change Error')
             };
-            request1Update.onsuccess = function(event) {
+            requestUpdate.onsuccess = function(event) {
                 playerTracker.innerText = `${currManager.innerText} drafted ${data.name}`;
             };
-        };  
+        }; 
+        settingsRequest.onerror = function(event) {
+            console.log('Manager Tracker Error');
+        };
+        settingsRequest.onsuccess = function(event) {
+            var data = event.target.result;
+            var numManagers = parseInt(data.numManagers) -1;
+            if((data.currManager === numManagers) || (data.currManager === 0) && (data.currRound!== 1)){
+                data.currRound = data.currRound += 1;
+            }
+            if(isEven(data.currRound)){
+                data.currManager = data.currManager -= 1;
+                var requestUpdate = settingsStore.put(data);
+            }else{
+                data.currManager = data.currManager += 1;
+                var requestUpdate = settingsStore.put(data);
+            }
+            requestUpdate.onerror = function(event) {
+                console.log('Manager Tracker Error')
+            };
+            requestUpdate.onsuccess = function(event) {
+                displayInfo();
+            };
+        } 
     };
+
+    function displayDraftedPlayer() {
+        
+    }
 };
+
+
+function isEven(value) {
+	if (value%2 == 0)
+		return true;
+	else
+		return false;
+}

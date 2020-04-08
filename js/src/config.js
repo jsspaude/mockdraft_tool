3f2de36780a64206d4c120c940205a4d46d390d4
Jess Spaude <jsspaude@gmail.com>
2020-04-08 11:50:37 -0700

    managerDisplayData solved

--- js/src/config.js
+++ js/src/config.js
@@ -1,15 +1,6 @@
 const draftDataURL = './js/draft_data.json';
 const stores = ['playerStore', 'managerStore', 'settingsStore'];
 
-
-const chunkArray = (myArray, chunkSize) => {
-  const results = [];
-  while (myArray.length) {
-    results.push(myArray.splice(0, chunkSize));
-  }
-  return results;
-};
-
 const isEven = (value) => { // HELPER FOR ROUND TRACKING
   if (value % 2 === 0) return true;
   return false;
@@ -32,5 +23,5 @@ const throttle = (func, limit) => {
 export default { draftDataURL };
 
 export {
-  draftDataURL, stores, chunkArray, isEven,
+  draftDataURL, stores, isEven,
 };
--- js/src/view.js
+++ js/src/view.js
@@ -1,33 +1,92 @@
 import { dbGetCursorData } from './db';
-import { chunkArray as configChunkArray } from './config';
 
-// USE CURSOR TO EXTRACT - (cursor.value.values, desired objectStore, keys
-async function viewData() {
-  const managerCursorKeys = ['managerNum', 'managerName'];
-  let managerDisplayData;
-  await dbGetCursorData('managerStore', managerCursorKeys)
-    .then((values) => configChunkArray(values, managerCursorKeys.length))
-    .then((result) => { managerDisplayData = result; });
-    console.log(managerDisplayData);
-  // await displayData(db, 'managerStore', document.querySelector('[data-js="managerContainer"]'));
-}
 
-function displayData(data, markup) {
+function displayData(data) {
+  // const cont = container;
+  // cont.innerHTML = markup(data);
 
 }
 
 function managerMarkup(data) {
   const markup = `
-  <article data-js='manager '>
-  <tr data-playerKey=${data} data-manager=${data}>
-  <td>${cursor.value.adp}</td>
-  <td>${cursor.value.name}</td>
-  <td>${cursor.value.pos}</td>
-  <td>${cursor.value.team}</td>
-  <button data-playerKey=${cursor.value.id} data-manager=${cursor.value.manager} data-js='draftBtn'>DRAFT</button>
-</tr>`;
+  <article data-manager=${data.managerNum}>
+    <table data-manager=${data.managerNum}>
+      <th>${data.managerName}</th>
+      <tr data-manager=${data.managerNum}>
+        <td value='qb' data-js='drafted'>QB</td>
+        <td value='qb' data-js='drafted'></td>
+      </tr>
+      <tr data-manager=${data.managerNum}>
+        <td value='rb' data-js='drafted'>RB</td>
+        <td value='rb' data-js='drafted'></td>
+      </tr>
+      <tr data-manager=${data.managerNum}>
+        <td value='rb' data-js='drafted'>RB</td>
+        <td value='rb' data-js='drafted'></td>
+      </tr>
+      <tr data-manager=${data.managerNum}>
+        <td value='wr' data-js='drafted'>WR</td>
+        <td value='wr' data-js='drafted'></td>
+      </tr>
+      <tr data-manager=${data.managerNum}>
+        <td value='wr' data-js='drafted'>WR</td>
+        <td value='wr' data-js='drafted'></td>
+      </tr>
+      <tr data-manager=${data.managerNum}>
+        <td value='te' data-js='drafted'>TE</td>
+        <td value='te' data-js='drafted'></td>
+      </tr>
+      <tr data-manager=${data.managerNum}>
+        <td value='k' data-js='drafted'>K</td>
+        <td value='k' data-js='drafted'></td>
+      </tr>
+      <tr data-manager=${data.managerNum}>
+        <td value='dst'>D/ST</td>
+        <td value='dst' data-js='drafted'></td>
+      </tr>
+      <tr data-manager=${data.managerNum}>
+        <td value='bench'>BENCH</td>
+        <td value='bench' data-js='drafted'></td>
+      </tr>
+      <tr data-manager=${data.managerNum}>
+        <td value='bench'>BENCH</td>
+        <td value='bench' data-js='drafted'></td>
+      </tr>
+      <tr data-manager=${data.managerNum}>
+        <td value='bench'>BENCH</td>
+        <td value='bench' data-js='drafted'></td>
+      </tr>
+      <tr data-manager=${data.managerNum}>
+        <td value='bench'>BENCH</td>
+        <td value='bench' data-js='drafted'></td>
+      </tr>
+      <tr data-manager=${data.managerNum}>
+        <td value='bench'>BENCH</td>
+        <td value='bench' data-js='drafted'></td>
+      </tr>
+    </table>
+  </article>`;
+  return markup;
 }
 
+// USE CURSOR TO EXTRACT - maybe change to collectCursorData and store in db file, 
+// break this and view into separate functions
+async function viewData() {
+  const managerCursorKeys = ['managerNum', 'managerName'];
+  const managerDisplayData = [];
+  await dbGetCursorData('managerStore', managerCursorKeys)
+    .then((values) => {
+      values.forEach((value, i) => {
+        if ((i - 1) % (managerCursorKeys.length)) {
+          const obj1 = { ...value };
+          const obj2 = { ...values[i + 1] };
+          const obj3 = { ...obj1, ...obj2 };
+          return managerDisplayData.push(obj3);
+        }
+        return managerDisplayData;
+      });
+    });
+}
 
 export default { viewData };
 export { viewData };

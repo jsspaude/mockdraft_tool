99d86cfab2ce44d1f699c23e4de9bebf6370a03b
Jess Spaude <jsspaude@gmail.com>
2020-08-18 15:42:26 -0700

    build deployed

---
+++ build/precache-manifest.9f57b1093d1e9c7f3032503d85429576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c7c2b9cfdda79b1cf33f0de8078577",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f202662dd3107ced1aad",
+    "url": "/static/css/main.4162d5a5.chunk.css"
+  },
+  {
+    "revision": "1c4808f005032c6dfaab",
+    "url": "/static/js/2.75a9f6d3.chunk.js"
+  },
+  {
+    "revision": "6e0f4d5b2989a9f224c24c390049b88f",
+    "url": "/static/js/2.75a9f6d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f202662dd3107ced1aad",
+    "url": "/static/js/main.e0691ac0.chunk.js"
+  },
+  {
+    "revision": "a2c5dba4d6525be8781f",
+    "url": "/static/js/runtime-main.d6abbe58.js"
+  }
+]);

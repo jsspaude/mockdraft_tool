9ba9f70b417a5c1e4db977e7a7818353b847f66d
Jess Spaude <jsspaude@gmail.com>
2020-10-08 15:33:09 -0700

    header style adjustments

--- src/components/Navigation/NavigationMobile.js
+++ src/components/Navigation/NavigationMobile.js
@@ -1,25 +1,31 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import { ReactComponent as LogoSvg } from '../../img/logo.svg';
 
 const useStyles = makeStyles({
   list: {
     width: 250,
+    height: '100%',
   },
   fullList: {
     width: 'auto',
   },
 });
 
-const NavigationMobile = () => {
+const Logo = () => <LogoSvg height="100px" width="200px" />;
+
+const NavigationMobile = (props) => {
   const classes = useStyles();
   const [state, setState] = React.useState({
     top: false,
@@ -45,25 +51,61 @@ const NavigationMobile = () => {
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
-      <List>
-        <ListItem button key="login">
-          <Link to="/login">
-            <ListItemText primary="Login" />
-          </Link>
+      <List style={{ backgroundColor: '#1c0f13' }}>
+        <ListItem key="logo">
+          <Logo />
         </ListItem>
-        <ListItem button key="signup">
-          <Link to="/signup">
-            <ListItemText primary="Signup" />
-          </Link>
-        </ListItem>
+      </List>
+      <List style={{ backgroundColor: '#d8dbe2', height: '100%' }}>
+        <Link to="/" className="reset" onClick={(e) => props.handleReset(e)}>
+          <ListItem style={{ borderBottom: '1px solid #f6e8ea', padding: 15 }} button key="reset">
+            <ListItemText
+              primary={
+                <Typography
+                  style={{
+                    color: '#1c0f13',
+                    fontFamily: 'Lato',
+                  }}
+                  variant="h6"
+                >
+                  Reset
+                </Typography>
+              }
+            />
+            <ArrowForwardIosIcon />
+          </ListItem>
+        </Link>
+        <Link to="/login" className="logout" onClick={(e) => props.handleLogout(e)}>
+          <ListItem style={{ borderBottom: '1px solid #f6e8ea', padding: 15 }} button key="logout">
+            <ListItemText
+              primary={
+                <Typography
+                  style={{
+                    color: '#1c0f13',
+                    fontFamily: 'Lato',
+                  }}
+                  variant="h6"
+                >
+                  Logout
+                </Typography>
+              }
+            />
+            <ArrowForwardIosIcon />
+          </ListItem>
+        </Link>
       </List>
     </div>
   );
 
+  NavigationMobile.propTypes = {
+    handleLogout: PropTypes.func,
+    handleReset: PropTypes.func,
+  };
+
   return (
     <div>
       <React.Fragment key="drawer">
-        <MenuIcon onClick={toggleDrawer('left', true)}></MenuIcon>
+        <MenuIcon style={{ color: 'white' }} onClick={toggleDrawer('left', true)}></MenuIcon>
         <SwipeableDrawer
           anchor={'left'}
           open={state.left}
